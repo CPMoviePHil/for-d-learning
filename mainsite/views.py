dc3cebbcaab46313f5080cf3d6603cd67a97ca84
cpmoviephil <you9215712@gmail.com>
2020-10-04 02:17:17 +0800

    +上urls controller

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse
 
 # Create your views here.
+def mainsite_index(request):
+    posts = Post.objects.all()
+    li = list()
+    for count, value in enumerate(posts):
+        li.append(f'No.{count}:{value}<br/>')
+    return HttpResponse(li)
