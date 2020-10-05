4bd311c0e0b5cb9fbac92c1fe5ab54669135bdb7
cpmoviephil <you9215712@gmail.com>
2020-10-05 20:19:06 +0800

    +views

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def mainsite_index(request):
     posts = Post.objects.all()
-    li = list()
-    for count, value in enumerate(posts):
-        li.append(f'No.{count}:{value}<br/>')
-    return HttpResponse(li)
+    # li = list()
+    # for count, value in enumerate(posts):
+    #     li.append(f'No.{count}:{value}<br/>')
+    
+    now = datetime.now()
+    context = {
+        'posts': posts,
+        'now': now,
+    }
+    return render(request, 'mainsite/index.html', context)
